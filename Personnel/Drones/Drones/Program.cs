02a1b06b67d20e6ef812b48eca3aa7cc27c2b2d3
Alessandro <pp67yxi@eduvaud.ch>
2024-09-13 12:11:49 +0200

    Update

--- Personnel/Drones/Drones/Program.cs
+++ Personnel/Drones/Drones/Program.cs
@@ -16,20 +16,26 @@ namespace Drones
 
             // Création de la flotte de drones
             List<Drone> fleet= new List<Drone>();
-            Drone drone = new Drone();
-            drone.X = RandomHelpers.alea.Next(0, AirSpace.WIDTH);
-            drone.Y = RandomHelpers.alea.Next(0, AirSpace.HEIGHT);
-            drone.Name = "Arthur";
-            fleet.Add(drone);
+            for (int i = 0; i < RandomHelpers.alea.Next(1, 10); i++)
+            {
+                Drone drone = new Drone();
+                drone.X = RandomHelpers.alea.Next(0, AirSpace.WIDTH);
+                drone.Y = RandomHelpers.alea.Next(0, AirSpace.HEIGHT);
+                drone.Name = "Arthur";
+                fleet.Add(drone);
+            }
 
             List<Building> buildings = new List<Building>();
-            Building building = new Building();
-            building.BuildingColor = Color.Black;
-            building.Width = 10;
-            building.Depth = 10;
-            building.X = RandomHelpers.alea.Next(0 + building.Width, AirSpace.WIDTH - building.Width);
-            building.Y = RandomHelpers.alea.Next(0 + building.Depth, AirSpace.HEIGHT - building.Depth);
-            buildings.Add(building);
+            for (int i = 0; i < RandomHelpers.alea.Next(1, 10); i++)
+            {
+                Building building = new Building();
+                building.BuildingColor = Color.Black;
+                building.Width = 10;
+                building.Depth = 10;
+                building.X = RandomHelpers.alea.Next(0 + building.Width, AirSpace.WIDTH - building.Width);
+                building.Y = RandomHelpers.alea.Next(0 + building.Depth, AirSpace.HEIGHT - building.Depth);
+                buildings.Add(building);
+            }
 
             // Démarrage
             Application.Run(new AirSpace(fleet, buildings));
